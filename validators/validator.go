6104ef0de4e566c1548b1aa5b0d47afe1de11906
oceano-dev <oceano.dev@outlook.com>
2022-05-16 11:53:34 -0300

    validators update

--- validators/validator.go
+++ validators/validator.go
@@ -2,29 +2,54 @@ package validators
 
 import (
 	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/pt_BR"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	en_translations "github.com/go-playground/validator/v10/translations/en"
 )
 
+type validators struct {
+	trans ut.Translator
+}
+
+var language string
+
 func NewValidator(
 	language string,
-) interface{} {
-	return func(data interface{}) interface{} {
-		uni := ut.New(en.New())
-		trans, _ := uni.GetTranslator(language)
-		validate := validator.New()
+) *validators {
+	var uni *ut.UniversalTranslator
+	var trans ut.Translator
 
-		err := en_translations.RegisterDefaultTranslations(validate, trans)
-		if err != nil {
-			errors := []string{}
-			for _, err := range err.(validator.ValidationErrors) {
-				errors = append(errors, err.Translate(trans))
-			}
+	switch language {
+	case "en":
+		uni = ut.New(en.New())
+	case "pt_BR":
+		uni = ut.New(pt_BR.New())
+	default:
+		uni = ut.New(en.New())
+	}
 
-			return errors
+	trans, _ = uni.GetTranslator(language)
+
+	return &validators{
+		trans: trans,
+	}
+}
+
+func (v *validators) Validate(data interface{}) interface{} {
+	uni := ut.New(en.New())
+	trans, _ := uni.GetTranslator(language)
+	validate := validator.New()
+
+	err := en_translations.RegisterDefaultTranslations(validate, trans)
+	if err != nil {
+		errors := []string{}
+		for _, err := range err.(validator.ValidationErrors) {
+			errors = append(errors, err.Translate(trans))
 		}
 
-		return nil
+		return errors
 	}
+
+	return nil
 }
