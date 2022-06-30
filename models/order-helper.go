5d3cb4a004f48bddd42f42e483b5b170b7cdbf80
oceano-dev <oceano.dev@outlook.com>
2022-06-30 14:48:50 -0300

    update orderhelper

--- models/order-helper.go
+++ models/order-helper.go
@@ -10,3 +10,22 @@ const (
 	OrderCreated                OrderStatus = 4
 	OrderCanceled               OrderStatus = 5
 )
+
+func (status OrderStatus) String() string {
+	switch status {
+	case SentForPaymentConfirmation:
+		return "Sent for payment confirmation"
+	case AwaitingPaymentConfirmation:
+		return "Awaiting payment confirmation"
+	case PaymentConfirmed:
+		return "Payment confirmed"
+	case PaymentRejected:
+		return "Payment rejected"
+	case OrderCreated:
+		return "Order created"
+	case OrderCanceled:
+		return "Order canceled"
+	}
+
+	return "unknown"
+}
