bdf3d71e94fb50e19049312f97e496b315aebb65
oceano-dev <oceano.dev@outlook.com>
2023-01-06 14:37:34 -0300

    cors middleware update

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,21 +1,20 @@
 package middlewares
 
 import (
-	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
-	return cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"POST, GET, PUT, PATCH, DELETE"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	})
+	return cors.Default()
+	// return cors.New(cors.Config{
+	// 	AllowAllOrigins:  true,
+	// 	AllowMethods:     []string{"POST, GET, PUT, PATCH, DELETE"},
+	// 	AllowHeaders:     []string{"Origin"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           12 * time.Hour,
+	// })
 
 	// return func(c *gin.Context) {
 	// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
