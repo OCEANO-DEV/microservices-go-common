c7f5f3d861cef99b579ad1cf3a7c45d265119d89
oceano-dev <oceano.dev@outlook.com>
2023-01-09 14:40:56 -0300

    update cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -9,10 +9,10 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"POST, GET, PUT, PATCH, DELETE"},
-		AllowHeaders:     []string{"Content-Type, application/json, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"},
-		ExposeHeaders:    []string{"Content-Length"},
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"POST, GET, PUT, PATCH, DELETE"},
+		AllowHeaders:    []string{"*"},
+		// ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	})
