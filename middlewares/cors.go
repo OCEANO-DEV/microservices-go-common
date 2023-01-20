f072993797927bb6ca8a89e537ac531ad059cbac
oceano-dev <oceano.dev@outlook.com>
2023-01-20 16:12:59 -0300

    update cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -10,7 +10,11 @@ import (
 func CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		// AllowAllOrigins:  true,
-		AllowOrigins:     []string{"http://localhost:3000", "https://localhost", "https://mymicroservices.com"},
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"https://localhost:5001",
+			"https://localhost",
+			"https://mymicroservices.com"},
 		AllowMethods:     []string{"GET, POST, PUT, PATCH, DELETE, OPTIONS"},
 		AllowHeaders:     []string{"Content-Type, Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
