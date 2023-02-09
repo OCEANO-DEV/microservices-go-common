5fc31c952734d92ae90e9160096b31fb39281873
oceano-dev <oceano.dev@outlook.com>
2023-02-09 16:54:21 -0300

    udpate helpers

--- helpers/mongo.go
+++ helpers/mongo.go
@@ -4,25 +4,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type ID primitive.ObjectID
-
-// String convert ID to string.
-func (id ID) String() string {
-	return primitive.ObjectID(id).Hex()
-}
-
-// StringToID converts a string to ID.
 func StringToID(s string) primitive.ObjectID {
 	id, _ := primitive.ObjectIDFromHex(s)
 	return id
 }
 
-// IsValidID checks if ID is valid.
 func IsValidID(s string) bool {
 	return primitive.IsValidObjectID(s)
-}
-
-// NewID create a new id
-func NewID() primitive.ObjectID {
-	return primitive.NewObjectID()
 }
