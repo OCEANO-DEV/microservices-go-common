51f625725ff07b2e7acb33e04256f0ad67fecfe9
oceano-dev <oceano.dev@outlook.com>
2023-03-31 06:39:10 -0300

    update common

--- grpc/grpc-server.go
+++ grpc/grpc-server.go
@@ -1,10 +1,11 @@
 package proto
 
 import (
+	"crypto/tls"
 	"time"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpcrecovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	grpc_otel "go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
@@ -13,20 +14,24 @@ import (
 	"github.com/oceano-dev/microservices-go-common/middlewares"
 	"github.com/oceano-dev/microservices-go-common/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
 )
 
 type GrpcServer struct {
 	config         *config.Config
+	service        services.CertificatesService
 	serviceMetrics services.Metrics
 }
 
 func NewGrpcServer(
 	config *config.Config,
+	service services.CertificatesService,
 	serviceMetrics services.Metrics,
 ) *GrpcServer {
 	return &GrpcServer{
 		config:         config,
+		service:        service,
 		serviceMetrics: serviceMetrics,
 	}
 }
@@ -39,15 +44,41 @@ func (s *GrpcServer) CreateGrpcServer() (*grpc.Server, error) {
 			MaxConnectionAge:  time.Duration(s.config.GrpcServer.MaxConnectionAge) * time.Minute,
 			Time:              time.Duration(s.config.GrpcServer.Timeout) * time.Minute,
 		}),
-		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
+		grpc.Creds(s.credentials()),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_ctxtags.StreamServerInterceptor(),
+			grpc_otel.StreamServerInterceptor(),
+			grpc_prometheus.StreamServerInterceptor,
+			grpc_recovery.StreamServerInterceptor(),
+			middlewares.StreamServerInterceptorMetrics(s.serviceMetrics),
+		),
+		),
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 			grpc_ctxtags.UnaryServerInterceptor(),
 			grpc_otel.UnaryServerInterceptor(),
 			grpc_prometheus.UnaryServerInterceptor,
-			grpcrecovery.UnaryServerInterceptor(),
-			middlewares.MetricsGRPC(s.serviceMetrics)),
+			grpc_recovery.UnaryServerInterceptor(),
+			middlewares.UnaryServerInterceptorMetrics(s.serviceMetrics)),
 		),
 	)
 
 	return grpcServer, nil
+}
+
+func (s *GrpcServer) credentials() credentials.TransportCredentials {
+	tls := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+		GetCertificate: s.service.GetLocalCertificate,
+		ClientCAs:      s.service.GetLocalCertificateCA(),
+	}
+
+	return credentials.NewTLS(tls)
 }
--- middlewares/metrics-grpc.go
+++ middlewares/metrics-grpc.go
@@ -11,7 +11,27 @@ import (
 	"google.golang.org/grpc"
 )
 
-func MetricsGRPC(service services.Metrics) grpc.UnaryServerInterceptor {
+func StreamServerInterceptorMetrics(service services.Metrics) grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+
+		status := http.StatusOK
+
+		err := handler(srv, stream)
+		if err != nil {
+			log.Println(err)
+		}
+
+		appMetric := metrics.NewHttpMetrics(info.FullMethod, "POST")
+		appMetric.Started()
+		appMetric.Finished()
+		appMetric.StatusCode = strconv.Itoa(status)
+		service.SaveHttp(appMetric)
+
+		return err
+	}
+}
+
+func UnaryServerInterceptorMetrics(service services.Metrics) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 
 		status := http.StatusOK
