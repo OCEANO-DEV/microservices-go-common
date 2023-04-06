d55c8fbf026fabbbb61324e4c3dfdbeb870c8cf4
oceano-dev <oceano.dev@outlook.com>
2023-04-06 10:08:47 -0300

    update common

--- nats/nats.go
+++ nats/nats.go
@@ -16,6 +16,9 @@ func NewNats(config *config.Config, service services.CertificatesService) (*nats
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
